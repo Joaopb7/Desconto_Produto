0802a80466a3971c5dda351deb9668eaba71727f
João Luiz Soares Alves <joao-luiz171@hotmail.com>
2025-09-24 20:54:11 -0300

    Atualização

--- Calc-desconto.py
+++ Calc-desconto.py
@@ -1,4 +1,4 @@
-
+debito = 0
 #função que calcula e retorna o preço final, de acordo com o desconto aplicado sobre o preço.
 def calcular_preco_final(preco, desconto):
     preco_final = preco - (preco * desconto/100)
@@ -8,9 +8,9 @@ def calcular_preco_final(preco, desconto):
 def mostrarLinhas():
     print('=-'*20)
 
+
 #laço de repetição para obter 3 produtos diferentes.
 for x in range (3):
-    debito = 0
 
     preco_original = float(input("Digite o preço do produto: "))
     preco_desconto = int(input("Digite o percentual de desconto: "))
